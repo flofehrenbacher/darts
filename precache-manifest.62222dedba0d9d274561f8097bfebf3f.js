302d0d5a17aae42cfbfbe35ac749fd59ad8f71d5
Florian Fehrenbacher <flo.fehrenbacher@icloud.com>
2020-06-06 22:15:50 +0200

    Updates

---
+++ precache-manifest.62222dedba0d9d274561f8097bfebf3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b6f6d5919d1e31147f0a2512aa6039",
+    "url": "/darts/index.html"
+  },
+  {
+    "revision": "d39a03bd2ca50a89203a",
+    "url": "/darts/static/css/main.749dd8d7.chunk.css"
+  },
+  {
+    "revision": "3f22c0ff894bf34a2a79",
+    "url": "/darts/static/js/2.d2c8eb4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/darts/static/js/2.d2c8eb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39a03bd2ca50a89203a",
+    "url": "/darts/static/js/main.06e99b18.chunk.js"
+  },
+  {
+    "revision": "8537b9ab9552d426b467",
+    "url": "/darts/static/js/runtime-main.4e9586ce.js"
+  }
+]);
