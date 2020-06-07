70fa178790deec361b79cdc10919bdd0684f67ce
Florian Fehrenbacher <flo.fehrenbacher@icloud.com>
2020-06-07 13:40:40 +0200

    Updates

--- precache-manifest.0b2c18d79d1a8f4f92699aae5e5f89f6.js
+++ precache-manifest.0b2c18d79d1a8f4f92699aae5e5f89f6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "57b6f6d5919d1e31147f0a2512aa6039",
+    "revision": "f6ae792cf6cad0d8955f6ffa9fa60158",
     "url": "/darts/index.html"
   },
   {
-    "revision": "d39a03bd2ca50a89203a",
+    "revision": "c701d6f818bef7d799b7",
     "url": "/darts/static/css/main.749dd8d7.chunk.css"
   },
   {
-    "revision": "3f22c0ff894bf34a2a79",
-    "url": "/darts/static/js/2.d2c8eb4b.chunk.js"
+    "revision": "ab4f9f3ad5e6caf0ec1d",
+    "url": "/darts/static/js/2.84c619a9.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/darts/static/js/2.d2c8eb4b.chunk.js.LICENSE.txt"
+    "url": "/darts/static/js/2.84c619a9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d39a03bd2ca50a89203a",
-    "url": "/darts/static/js/main.06e99b18.chunk.js"
+    "revision": "c701d6f818bef7d799b7",
+    "url": "/darts/static/js/main.d699559e.chunk.js"
   },
   {
     "revision": "8537b9ab9552d426b467",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/darts/precache-manifest.62222dedba0d9d274561f8097bfebf3f.js"
+  "/darts/precache-manifest.0b2c18d79d1a8f4f92699aae5e5f89f6.js"
 );
 
 self.addEventListener('message', (event) => {
