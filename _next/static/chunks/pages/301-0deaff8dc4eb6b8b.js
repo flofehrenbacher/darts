826e64ca6b88cef9d7c0026494610221cbdd7df2
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2021-11-20 12:36:12 +0100

    Updates

---
+++ _next/static/chunks/pages/301-0deaff8dc4eb6b8b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[112],{3360:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/301",function(){return u(5625)}])}},function(n){n.O(0,[630,983,511,942,670,950,505,774,888,179],(function(){return _=3360,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
